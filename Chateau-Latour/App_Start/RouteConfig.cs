98a41f08aa7dc68ad5d8c23b4272b8b30267df48
2407216686@qq.com <2407216686@qq.com>
2019-06-12 15:03:25 +0800

    1

--- Chateau-Latour/App_Start/RouteConfig.cs
+++ Chateau-Latour/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Chateau_Latour
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Single", action = "Single", id = UrlParameter.Optional }
             );
         }
     }
