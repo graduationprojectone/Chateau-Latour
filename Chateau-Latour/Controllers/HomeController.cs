5d3c139e4e5f3ee712cf6db573beacf6191b09b2
2407216686@qq.com <2407216686@qq.com>
2019-06-14 14:37:06 +0800

    登录

--- Chateau-Latour/Controllers/HomeController.cs
+++ Chateau-Latour/Controllers/HomeController.cs
@@ -19,7 +19,15 @@ namespace Chateau_Latour.Controllers
         {
             //查询商品
             LaTuErEntities db = new LaTuErEntities();
-            var list = db.E_Commodity.ToList();
+            var list = db.E_Commodity.ToList()
+                .Select(c => new
+                {
+                    CommodityId = c.CommodityId,
+                    Tradename = c.Tradename,
+                    SubtitleofCommodity = c.SubtitleofCommodity,
+                    Price = c.Price,
+                    Image = c.Image
+                }).ToList();
             return Json(list,JsonRequestBehavior.AllowGet);
         }
     }
--- Chateau-Latour/Controllers/LoginController.cs
+++ Chateau-Latour/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Chateau_Latour.Models;
 
 namespace Chateau_Latour.Controllers
 {
@@ -12,6 +13,20 @@ namespace Chateau_Latour.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string txt_id, string txt_pwd)
+        {
+            LaTuErEntities db = new LaTuErEntities();
+            var emp = db.A_UserLogin
+                .Where(c => c.UserPhone == txt_id && c.UserPwd == txt_pwd)
+                .FirstOrDefault();
+            if (emp != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Login");
         }
     }
 }
