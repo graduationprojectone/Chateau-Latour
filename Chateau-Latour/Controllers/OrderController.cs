7d717bd18b74ff91dc1896c3ba73cfb508623318
2407216686@qq.com <2407216686@qq.com>
2019-06-15 20:57:04 +0800

    订单界面高了半天没搞出来。我要睡了。再见

--- Chateau-Latour/Controllers/OrderController.cs
+++ Chateau-Latour/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Chateau_Latour.Models;
 
 namespace mvc2.Controllers
 {
@@ -13,5 +14,6 @@ namespace mvc2.Controllers
         {
             return View();
         }
+
     }
 }
